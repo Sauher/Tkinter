12872b88577134f394cf02bb433bc950b7606e9f
n-szilard <nagyapatiszilard@turr.hu>
2023-05-31 12:30:08 +0200

    Akasztófa új játék függvény

--- akasztofa.py
+++ akasztofa.py
@@ -4,6 +4,10 @@ from random import choice
 
 def akasztofa_toplevel(foablak):
 
+    global szo
+    global hibak
+    global kitalalt_betuk
+
     def random_szo():
         szavak = open("szavak.txt").read().splitlines()
         random_szo = choice(szavak)
@@ -20,6 +24,14 @@ def akasztofa_toplevel(foablak):
     oszlop = 0
     sor = 1
     
+    def uj_jatek():
+        global szo
+        global hibak
+        global kitalalt_betuk
+        szo = random_szo()
+        hibak = 0
+        kitalalt_betuk = []
+    
     for betu in ascii_lowercase:
         gombok = Button(top, text=betu, command= lambda x = betu: betu_bevitel(x))
         gombok.grid(row=sor, column=oszlop)
@@ -27,5 +39,9 @@ def akasztofa_toplevel(foablak):
         if oszlop % 8 == 0:
             oszlop = 0
             sor += 1
+
+    uj_jatek_gomb = Button(top, text="Új játék", command=uj_jatek)
+
+    uj_jatek()
 
 
